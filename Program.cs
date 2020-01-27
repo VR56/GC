20c44477719ffcc1f8af06fec1f9ae0e5acc8390
VR56 <53446898+VR56@users.noreply.github.com>
2020-01-27 18:05:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyResources r = null;
+
+            try
+            {
+                r = new MyResources(@"C:\Users\Vishal_Tulugu\Desktop\Test.txt");
+                r.ShowData();
+                Console.ReadLine();
+            }
+            finally
+            {
+                r.Dispose();
+                Console.ReadLine(); ;
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    class MyResources : IDisposable
+    {
+        TextReader tr = null;
+
+        public MyResources(string path)
+        {
+            Console.WriteLine("Aquiring Managed Resources");
+            tr = new StreamReader(path);
+
+            Console.WriteLine("Aquiring Unmanaged Resources");
+            
+
+        }
+
+        void ReleaseManagedResources()
+        {
+            Console.WriteLine("Releasing Managed Resources");
+            if (tr != null)
+            {
+                tr.Dispose();
+            }
+        }
+
+        void ReleaseUnmangedResources()
+        {
+            Console.WriteLine("Releasing Unmanaged Resources");
+        }
+
+        public void ShowData()
+        {
+
+            if (tr != null)
+            {
+                Console.WriteLine(tr.ReadToEnd() + " /some unmanaged data ");
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose called from outside");
+
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            Console.WriteLine("Actual Dispose called with a " + disposing.ToString());
+            if (disposing == true)
+            {
+                ReleaseManagedResources();
+            }
+            else
+            {
+            }
+
+            ReleaseUnmangedResources();
+
+        }
+        /// <summary>
+        /// Destructor
+        /// </summary>
+        ~MyResources()
+        {
+            Console.WriteLine("Finalizer called");
+            Dispose(false);
+        }
+    }
+}
