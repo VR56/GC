ee024b9fc70b509e2774a8b8cf34be076d8545e0
VR56 <53446898+VR56@users.noreply.github.com>
2020-01-29 00:46:41 +0530

    VList

--- Program.cs
+++ Program.cs
@@ -4,25 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StudentDatabase
+namespace VList
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MyResources r = null;
+            VList LL = new VList();
 
-            try
-            {
-                r = new MyResources(@"C:\Users\Vishal_Tulugu\Desktop\Test.txt");
-                r.ShowData();
-                Console.ReadLine();
-            }
-            finally
-            {
-                r.Dispose();
-                Console.ReadLine(); ;
-            }
+            LL.Add(LL, 3,1);
+            LL.Add(LL, 4,2);
+            LL.Add(LL, 5,3);
+            LL.Add(LL, 6,4);
+            
+            LL.Remove(LL, 2);
+            LL.Print(LL);
+            Console.WriteLine(LL.Search(LL,5));
+            Console.WriteLine(LL.KSearch(LL, 5));
+            Console.WriteLine(LL.DSearch(LL, 1));
+            Console.ReadLine();
         }
     }
 }
---
+++ VList.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace VList
+{
+    internal class VList
+        {
+            internal Node head;
+
+            public class Node
+            {
+                internal int data;
+                internal int key;
+                internal Node next;
+                public Node(int data,int key)
+                {
+
+                    this.key = key;
+                    this.data = data;
+                    next = null;
+                }
+        }
+
+            internal void Add(VList vlist, int new_data,int new_key)
+            {
+                Node new_node = new Node(new_data,new_key);
+                if (vlist.head == null)
+                {
+                    vlist.head = new_node;
+                    return;
+                }
+                Node lastNode = GetLastNode(vlist);
+                lastNode.next = new_node;
+            }
+
+            internal Node GetLastNode(VList vList)
+            {
+                Node temp = vList.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                return temp;
+            }
+
+            internal void Remove(VList vList, int key)
+            {
+                Node temp = vList.head;
+                Node prev = null;
+                if (temp != null && temp.key == key)
+                {
+                    vList.head = temp.next;
+                    return;
+                }
+                while (temp != null && temp.key != key)
+                {
+                    prev = temp;
+                    temp = temp.next;
+                }
+                if (temp == null)
+                {
+                    return;
+                }
+                prev.next = temp.next;
+            }
+
+
+            internal void Print(VList vList)
+            {
+                Node n = vList.head;
+                while (n != null)
+                {
+                    Console.Write("Key: " + n.key);
+                    Console.WriteLine("Data: " + n.data + " ");
+                    n = n.next;
+                }
+           // Console.ReadLine();
+            }
+
+            internal bool Search(VList vList, int data)
+            {
+                Node n = vList.head;
+                int flag = 0;
+            while (n != null)
+                {
+                    if (n.data == data)
+                    {
+                        flag = 1;
+                        
+                    }
+                    n = n.next;
+                }
+             if (flag == 1)
+                return true;
+             else
+                return false;
+            }
+
+        internal int KSearch(VList vList, int data)
+        {
+            Node n = vList.head;
+            int flag = 0;
+            int key = 0;
+            while (n != null)
+            {
+                if (n.data == data)
+                {
+                    key = n.key;
+                    flag = 1;
+                }
+                n = n.next;
+            }
+            if (flag == 1)
+                return key;
+            else
+                return 999;
+        }
+
+        internal int DSearch(VList vList, int key)
+        {
+            Node n = vList.head;
+            int flag = 0;
+            int data = 0;
+            while (n != null)
+            {
+                if (n.key == key)
+                {
+                    data = n.data;
+                    flag = 1;
+                }
+                n = n.next;
+            }
+            if (flag == 1)
+                return data;
+            else
+                return 999;
+        }
+
+
+    }
+
+   
+}
